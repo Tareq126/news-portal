04bb032ec6d8d53271b13c7106e8c533cba38a7d
Tareq126 <tareq.12610@gmail.com>
2022-12-07 20:01:23 +0600

    Some design improved

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ const displayCategoryInfo = infos => {
 
         infoDiv.innerHTML = `
         <div class="card mb-3" style="max-width: 1400px;">
-                <div class="row g-0">
+                <div class="row g-0 border border-dark border-1">
                     <div class="col-lg-4 col-md-6 p-5">
                         <img src="${info.thumbnail_url}" class="img-fluid" alt="...">
                     </div>
@@ -60,7 +60,7 @@ const displayCategoryInfo = infos => {
                             <p class="card-text cut-text">Details: ${info.details.slice(0, 500)}... </p>
                         </div>
                        <div class="text-center">
-                       <button onclick="loadNewsDetails('${info._id}')" type="button" class="mb-2 px-5 rounded-3 text-center" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+                       <button onclick="loadNewsDetails('${info._id}')" type="button" class="mb-2 px-5 py-2 rounded-2 bg-secondary border-3 border-bottom border-dark border-top-0 border-start-0 border-end-0" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
                        </div>
                     </div>
                 </div>
