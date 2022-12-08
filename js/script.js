30e87faafd23a155a27d3106b1fea667594599c9
Tareq126 <tareq.12610@gmail.com>
2022-12-08 12:23:43 +0600

    Blog Section added

--- js/script.js
+++ js/script.js
@@ -63,16 +63,16 @@ const displayCategoryInfo = infos => {
         infoDiv.innerHTML = `
         <div class="card mb-3" style="max-width: 1400px;">
                 <div class="row g-0 border border-dark border-1">
-                    <div class="col-lg-4 col-md-6 p-5">
+                    <div class="col-lg-3 col-md-6">
                         <img src="${info.thumbnail_url}" class="img-fluid" alt="...">
                     </div>
-                    <div class="col-lg-8 col-md-6 py-5">
+                    <div class="col-lg-9 col-md-6 pt-5">
                         <div class="card-body">
                             <h5 class="card-title">${info.title}</h5>
                             <p class="card-text cut-text">Details: ${info.details.slice(0, 500)}... </p>
                         </div>
                        <div class="text-center">
-                       <button onclick="loadNewsDetails('${info._id}')" type="button" class="mb-2 px-5 py-2 rounded-2 bg-secondary border-3 border-bottom border-dark border-top-0 border-start-0 border-end-0" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+                       <button onclick="loadNewsDetails('${info._id}')" type="button" class="mb-1 px-5 py-2  rounded-2 bg-secondary border-3 border-bottom border-dark border-top-0 border-start-0 border-end-0" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
                        </div>
                     </div>
                 </div>
